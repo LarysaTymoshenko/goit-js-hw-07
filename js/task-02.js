38e4cf3955e704d76684925ec16f44befbf7b7c7
LarysaTymoshenko <luda76@ukr.net>
2021-09-04 16:06:09 +0300

    AppItem

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,15 @@ const ingredients = [
   'Приправы',
 ];
 
-const listEl = document.createElement(ingredients);
+const listEl = document.querySelector('#ingredients');
+// console.log(listEl);
+const makeElement = ingredients.map(element => {
+    const newElement = document.createElement('li');
+    newElement.textContent = element;
+    return newElement;
+
+})
+
+
+// console.log(makeElement);
+listEl.append(...makeElement);
