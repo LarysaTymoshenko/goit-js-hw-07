db7e9af3e2c3d15e1cce03f223c10ad863c700b0
LarysaTymoshenko <luda76@ukr.net>
2021-09-05 23:44:45 +0300

    today

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,14 @@
-const nameInput = document.querySelector('#name-input')
-const nameOutput = document.querySelector('#name-output')
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', onInputName);
+
+
+function onInputName(event) {
+ if(event.currentTarget.value){
+    nameOutput.textContent=event.currentTarget.value
+     return;
+    }
+    nameOutput.textContent = 'незнакомец';
+}
+
