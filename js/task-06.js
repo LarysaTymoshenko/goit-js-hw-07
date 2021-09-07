db4a2b78252d1e3f341555a499007d2ecdaca8b0
LarysaTymoshenko <luda76@ukr.net>
2021-09-07 16:34:17 +0300

    1634

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,29 @@
 const inputLength = document.querySelector('[data-length]');
 
 inputLength.addEventListener("blur", (number) => {
-    console.log(number);
-    if (Number(number.target.dataset.length) === inputLength..attributes["data-length"].number(value)
-)
+    // console.log(Number(inputLength.attributes["data-length"].value));
+
+    if (Number(number.target.dataset.length) >= Number(inputLength.attributes["data-length"].value)) { inputLength.classList.add('valid') };
+    else{
+    inputLength.classList.replace('valid', 'invalid')}
+})
+ 
+ 
+ 
+ 
+// function  onCheckDataLength(event) {
+//     Number(event.currentTarget.dataset.length) === event.currentTarget.value.length ?
+//         setClass('valid', 'invalid') :
+//         setClass('invalid', 'valid')
+// }
+
+// function setClass(add, remove) {
+//   refs.input.classList.add(add)
+//   refs.input.classList.remove(remove)
+// }
+// // 
+
+
 
 
  
