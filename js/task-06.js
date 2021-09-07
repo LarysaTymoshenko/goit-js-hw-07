358b6403149ab7bc2ba922f75d6388ffa9953694
LarysaTymoshenko <luda76@ukr.net>
2021-09-07 14:34:58 +0300

    1434

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputLength = document.querySelector('[data-length]');
+
+inputLength.addEventListener("blur", (number) => {
+
+
+    if (Number(number.target.dataset.length) === number.target.dataset.length) {
+
+        
+        
+     
+    }
+})
+
+ 
