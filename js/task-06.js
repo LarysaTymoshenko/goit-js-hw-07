0515422b1a1e0435943a4be2a099109711edce5e
LarysaTymoshenko <luda76@ukr.net>
2021-09-08 22:57:41 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,27 @@ const inputLength = document.querySelector('[data-length]');
 inputLength.addEventListener("blur", (number) => {
     // console.log(Number(inputLength.attributes["data-length"].value));
 
-    if (number.target.value.length >= Number(inputLength.attributes["data-length"].value)) {
-        inputLength.classList.add('valid');
-        inputLength.classList.replace('invalid', 'valid');
+    if (number.target.value.length === Number(inputLength.attributes["data-length"].value)) {
+        newClass('valid');
+        newClass('invalid', 'valid');
+       
+        // inputLength.classList.add('valid');
+        // inputLength.classList.replace('invalid', 'valid');
+
     }
     else {
- inputLength.classList.add('invalid');
-    inputLength.classList.replace('valid', 'invalid')}
+newClass('invalid');
+newClass('valid', 'invalid');
+//  inputLength.classList.add('invalid');
+ //     inputLength.classList.replace('valid', 'invalid')
+    }
 })
+
+
+function newClass(add, replase) {
+    inputLength.classList.add(add);
+    inputLength.classList.replace(replase);
+}
  
  
  
