9e72c1677781787762b0883af78df4d917ae768c
LarysaTymoshenko <luda76@ukr.net>
2021-09-08 23:06:33 +0300

    fix,set function

--- js/task-06.js
+++ js/task-06.js
@@ -2,28 +2,25 @@ const inputLength = document.querySelector('[data-length]');
 
 inputLength.addEventListener("blur", (number) => {
     // console.log(Number(inputLength.attributes["data-length"].value));
+    function newClass(add, replase) {
+   inputLength.classList.add(add);
+    inputLength.classList.replace(replase);
+}
 
     if (number.target.value.length === Number(inputLength.attributes["data-length"].value)) {
-        newClass('valid');
-        newClass('invalid', 'valid');
-       
-        // inputLength.classList.add('valid');
-        // inputLength.classList.replace('invalid', 'valid');
+
+        newClass('valid', 'invalid'); 
 
     }
     else {
-newClass('invalid');
-newClass('valid', 'invalid');
-//  inputLength.classList.add('invalid');
- //     inputLength.classList.replace('valid', 'invalid')
+
+newClass('invalid', 'valid');
+
     }
 })
 
 
-function newClass(add, replase) {
-    inputLength.classList.add(add);
-    inputLength.classList.replace(replase);
-}
+
  
  
  
